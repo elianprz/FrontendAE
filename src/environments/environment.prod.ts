45b4759705ec2ccb1befa3f0a5280cbc18652c01
elianprz <eperezr35@miumg.edu.gt>
2025-10-14 21:53:52 -0600

    cambio de url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://backendae.duckdns.org/api' // ?? REEMPLAZA con la URL real de tu backend
+  apiUrl: 'https://backendae.duckdns.org/api' // ?? REEMPLAZA con la URL real de tu backend
 };
